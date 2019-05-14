7b10fe83d3d0a66d643824023128446bbcc762c8
Yaroslav Sivakov <yar3333@gmail.com>
2019-05-14 16:44:25 +0300

    * v1.4.9

--- lib/nanodb/orm/FieldDeserializer.php
+++ lib/nanodb/orm/FieldDeserializer.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace nanodb\orm;
+
 use \nanodb\orm\EntityDeserializationException;
 
 class FieldDeserializer
@@ -14,13 +16,18 @@ class FieldDeserializer
 	private $rangeMin;
 	private $rangeMax;
 
-	function __construct(array $data, string $property)
+	function __construct(array $data, string $field)
 	{
 		$this->data = $data;
-		$this->property = $property;
+		$this->field = $field;
 	}
 
-	function optional($defaultValue) : self
+    /**
+     * Value used if field not present or null.
+     * @param mixed $defaultValue
+     * @return FieldDeserializer
+     */
+	function optional($defaultValue=null) : self
 	{
 		$this->optional = true;
 		$this->defaultValue = $defaultValue;
@@ -35,122 +42,65 @@ class FieldDeserializer
 		return $this;
 	}
 
-	function asMixedOrNull()
-	{
-        if (array_key_exists($this->property, $this->data) && $this->data[$this->property] === null) return null;
-        return $this->asMixed();
-    }
-
 	function asMixed()
 	{
 		if (!array_key_exists($this->property, $this->data)) {
 			if ($this->optional) return $this->defaultValue;
-			throw new EntityDeserializationException("Field [" . $this->property . "] is required", '__fromJson');
+			throw new EntityDeserializationException("Field [" . $this->field . "] is required", '__fromJson');
 		}
 
 		$v = $this->data[$this->property];
 		if ($v === null) {
 		    if ($this->optional) return $this->defaultValue;
-            throw new EntityDeserializationException("Null is not allowed for field [" . $this->property . "]", '__fromJson');
+            throw new EntityDeserializationException("Null is not allowed for field [" . $this->field . "]", '__fromJson');
         }
 
         return $v;
 	}
-
-	function asStringOrNull() : ?string
-	{
-        if (array_key_exists($this->property, $this->data) && $this->data[$this->property] === null) return null;
-        return $this->asString();
-    }
 
 	function asString() : string
 	{
-		if (!array_key_exists($this->property, $this->data)) {
-			if ($this->optional) return $this->defaultValue;
-			throw new EntityDeserializationException("Field [" . $this->property . "] is required", '__fromJson');
-		}
+        $v = $this->asMixed();
+		if ($this->optional && ($v === null || !array_key_exists($this->property, $this->data))) return $this->defaultValue;
 
-		$v = $this->data[$this->property];
-		if ($v === null) {
-		    if ($this->optional) return $this->defaultValue;
-            throw new EntityDeserializationException("Null is not allowed for field [" . $this->property . "]", '__fromJson');
-        }
-		if (!is_string($v)) throw new EntityDeserializationException("String is expected for field [" . $this->property . "]", '__fromJson');
+		if (!is_string($v)) throw new EntityDeserializationException("String is expected for field [" . $this->field . "]", '__fromJson');
+
         return $v;
 	}
 
-	function asIntOrNull() : ?int
-	{
-        if (array_key_exists($this->property, $this->data) && $this->data[$this->property] === null) return null;
-        return $this->asInt();
-    }
-
 	function asInt() : int
 	{
-		if (!array_key_exists($this->property, $this->data)) {
-			if ($this->optional) return $this->defaultValue;
-			throw new EntityDeserializationException("Field [" . $this->property . "] is required", '__fromJson');
-		}
+        $v = $this->asMixed();
+		if ($this->optional && ($v === null || !array_key_exists($this->property, $this->data))) return $this->defaultValue;
 
-		$v = $this->data[$this->property];
-		if ($v === null) {
-		    if ($this->optional) return $this->defaultValue;
-            throw new EntityDeserializationException("Null is not allowed for field [" . $this->property . "]", '__fromJson');
-        }
-		if (!is_numeric($v) || !ctype_digit($v)) throw new EntityDeserializationException("Integer is expected for field [" . $this->property . "]", '__fromJson');
+		if (!is_numeric($v) || !ctype_digit($v)) throw new EntityDeserializationException("Integer is expected for field [" . $this->field . "]", '__fromJson');
+
         $f = (float)$v;
-        if ($f < PHP_INT_MIN || $f > PHP_INT_MAX) throw new EntityDeserializationException("Value out of integer range for field [" . $this->property . "]", '__fromJson');
+        if ($f < PHP_INT_MIN || $f > PHP_INT_MAX) throw new EntityDeserializationException("Value out of integer range for field [" . $this->field . "]", '__fromJson');
 
         $i = (int)$v;
-        if ($this->checkRange && ($i < $this->rangeMin || $i > $this->rangeMax)) throw new EntityDeserializationException("Value out of alolowed range (" . $this->rangeMin . ".." . $this->rangeMax . ") for field [" . $this->property . "]", '__fromJson');
+        if ($this->checkRange && ($i < $this->rangeMin || $i > $this->rangeMax)) throw new EntityDeserializationException("Value out of allowed range " . $this->rangeMin . ".." . $this->rangeMax . " for field [" . $this->field . "]", '__fromJson');
 
         return $i;
 	}
 
-	function asFloatOrNull() : ?float
-	{
-        if (array_key_exists($this->property, $this->data) && $this->data[$this->property] === null) return null;
-        return $this->asFloat();
-    }
-
 	function asFloat() : float
 	{
-		if (!array_key_exists($this->property, $this->data)) {
-			if ($this->optional) return $this->defaultValue;
-			throw new EntityDeserializationException("Field [" . $this->property . "] is required", '__fromJson');
-		}
+        $v = $this->asMixed();
+		if ($this->optional && ($v === null || !array_key_exists($this->property, $this->data))) return $this->defaultValue;
 
-		$v = $this->data[$this->property];
-		if ($v === null) {
-		    if ($this->optional) return $this->defaultValue;
-		    throw new EntityDeserializationException("Null is not allowed for field [" . $this->property . "]", '__fromJson');
-        }
-		if (!is_numeric($v)) throw new EntityDeserializationException("Float is expected for field [" . $this->property . "]", '__fromJson');
+		if (!is_numeric($v)) throw new EntityDeserializationException("Float is expected for field [" . $this->field . "]", '__fromJson');
 
 		$f =  (float)$v;
-        if ($this->checkRange && ($f < $this->rangeMin || $f > $this->rangeMax)) throw new EntityDeserializationException("Value out of alolowed range (" . $this->rangeMin . ".." . $this->rangeMax . ") for field [" . $this->property . "]", '__fromJson');
+        if ($this->checkRange && ($f < $this->rangeMin || $f > $this->rangeMax)) throw new EntityDeserializationException("Value out of alolowed range (" . $this->rangeMin . ".." . $this->rangeMax . ") for field [" . $this->field . "]", '__fromJson');
 
         return $f;
 	}
 
-	function asDateTimeOrNull() : ?\DateTime
-	{
-        if (array_key_exists($this->property, $this->data) && $this->data[$this->property] === null) return null;
-        return $this->asDateTime();
-    }
-
 	function asDateTime() : \DateTime
 	{
-		if (!array_key_exists($this->property, $this->data)) {
-			if ($this->optional) return $this->defaultValue;
-			throw new EntityDeserializationException("Field [" . $this->property . "] is required", '__fromJson');
-		}
-
-		$v = $this->data[$this->property];
-		if ($v === null) {
-		    if ($this->optional) return $this->defaultValue;
-		    throw new EntityDeserializationException("Null is not allowed for field [" . $this->property . "]", '__fromJson');
-        }
+        $v = $this->asMixed();
+		if ($this->optional && ($v === null || !array_key_exists($this->property, $this->data))) return $this->defaultValue;
 
 		try
         {
@@ -168,6 +118,16 @@ class FieldDeserializer
         }
 		catch (\Exception $e) {}
 
-        throw new EntityDeserializationException("Date/time is expected for field [" . $this->property . "]", '__fromJson');
+        throw new EntityDeserializationException("Date/time is expected for field [" . $this->field . "]", '__fromJson');
+	}
+
+	function asArray() : array
+	{
+        $v = $this->asMixed();
+		if ($this->optional && ($v === null || !array_key_exists($this->property, $this->data))) return $this->defaultValue;
+
+        if (!is_array($v)) throw new EntityDeserializationException("Array is expected for field [" . $this->field . "]", '__fromJson');
+
+		return (array)$v;
 	}
 }
