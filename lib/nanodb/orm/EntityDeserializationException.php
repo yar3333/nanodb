ec40111e8d92da07765659de4baee6d315eba582
Yaroslav Sivakov <yar3333@gmail.com>
2019-05-17 16:52:11 +0300

    * v1.5.8

--- lib/nanodb/orm/EntityDeserializationException.php
+++ lib/nanodb/orm/EntityDeserializationException.php
@@ -6,9 +6,9 @@ class EntityDeserializationException extends \Exception
 {
 	public $methodSuffix;
 
-	function __construct(string $message, string $methodSuffix)
+	function __construct(string $message, string $methodSuffix, \Throwable $previous = null)
 	{
-		parent::__construct($message);
+		parent::__construct($message, 0, $previous);
 		
 		$this->methodSuffix = $methodSuffix;
 	}
