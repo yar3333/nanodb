2720b469cd1f54780d54e0805227fc438ecc561c
Yaroslav Sivakov <yar3333@gmail.com>
2019-05-17 11:58:50 +0300

    * v1.5.5

--- lib/nanodb/orm/Entity.php
+++ lib/nanodb/orm/Entity.php
@@ -5,37 +5,44 @@ namespace nanodb\orm;
 class Entity implements \JsonSerializable
 {
     /**
-	 * param string $methodSuffix
-	 * param array $dest
-	 * param string $property
-     * @throws EntityFieldNotFoundException
-	 */
-    private function serializeProperty(string $methodSuffix, array &$dest, string $property) : void
+     * @param array $dest
+     * @param string $property
+     * @param string $methodSuffix
+     */
+    private function serializeProperty(array &$dest, string $property, string $methodSuffix) : void
     {
         $method = $property . $methodSuffix;
         if (method_exists($this, $method)) $this->$method($dest, $property);
-        else {
-			$dest[$property] = $this->$property;
-		}
+        else 			                   $this->serializePropertyDefault($dest, $property, $methodSuffix);
+    }
+
+    protected function serializePropertyDefault(array &$dest, string $property, string $methodSuffix) : void
+    {
+    	$dest[$property] = $this->$property;
     }
 
     /**
-	 * param string $methodSuffix
-	 * param string $src
-	 * param string $property
-     * @throws EntityFieldNotFoundException
-	 */
-	private function deserializeProperty(string $methodSuffix, array $src, string $property) : void
+     * @param array $src
+     * @param string $property
+     * @param string $methodSuffix
+     * @throws EntityDeserializationException
+     */
+    private function deserializeProperty(array $src, string $property, string $methodSuffix) : void
     {
         $method = $property . $methodSuffix;
 		if (method_exists($this, $method)) $this->$method($src, $property);
-		else
-		{
-			if (!array_key_exists($property, $src) || $methodSuffix === "__fromJson" && $src[$property] === null) throw new EntityDeserializationException("Field [" . $property . "] is required", $methodSuffix);
-			$this->$property = $src[$property];
-		}
+		else			                   $this->deserializePropertyDefault($src, $property, $methodSuffix);
     }
-	
+
+    protected function deserializePropertyDefault(array $src, string $property, string $methodSuffix) : void
+    {
+        if (!array_key_exists($property, $src) || $methodSuffix === "__fromJson" && $src[$property] === null)
+        {
+            throw new EntityDeserializationException("Field [" . $property . "] is required", $methodSuffix);
+        }
+        $this->$property = $src[$property];
+    }
+
     protected function serializePropertyIgnoreNull(array &$data, string $property) : void
     {
         if (property_exists($this, $property)) $data[$property] = $this->$property;
@@ -43,41 +50,41 @@ class Entity implements \JsonSerializable
 	
     public function jsonSerialize(array $properties=null) : array
     {
-        if ($properties == null) $properties = array_diff(array_keys(get_object_vars($this)), [ "db", "orm" ]);
+        if ($properties === null) $properties = array_keys(get_object_vars($this));
 
         $data = [];
         foreach ($properties as $var) {
-            $this->serializeProperty("__toJson", $data, $var);
+            $this->serializeProperty($data, $var, "__toJson");
         }
         return $data;
     }
 
     public function jsonDeserialize(array $data, array $properties=null) : void
     {
-		if ($properties == null) $properties = array_diff(array_keys(get_object_vars($this)), [ "db", "orm" ]);
+		if ($properties === null) $properties = array_keys(get_object_vars($this));
 
 		foreach ($properties as $var) {
-            $this->deserializeProperty("__fromJson", $data, $var);
+            $this->deserializeProperty($data, $var, "__fromJson");
         }
     }
 
 	public function dbSerialize(array $properties=null): array
     {
-        if ($properties == null) $properties = array_keys(get_object_vars($this));
+        if ($properties === null) $properties = array_keys(get_object_vars($this));
 
         $data = [];
         foreach ($properties as $var) {
-			$this->serializeProperty("__toDb", $data, $var);
+			$this->serializeProperty($data, $var, "__toDb");
         }
         return $data;
     }
 
     public function dbDeserialize(array $data, array $properties=null) : void
     {
-		if ($properties == null) $properties = array_keys(get_object_vars($this));
+		if ($properties === null) $properties = array_keys(get_object_vars($this));
         
 		foreach ($properties as $var) {
-            $this->deserializeProperty("__fromDb", $data, $var);
+            $this->deserializeProperty($data, $var, "__fromDb");
         }
     }
 }
